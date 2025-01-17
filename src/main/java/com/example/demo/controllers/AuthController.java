7569ebc903e74bec1780058606fc214c5f3be0cd
BranHill21 <bhill20@austincollege.edu>
2025-01-17 15:23:00 -0600

    start of adding auth. Auth helper classes

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import helpers.LoginRequest;
+import helpers.LoginResponse;
 
 @Controller
 public class AuthController {
@@ -14,6 +20,11 @@ public class AuthController {
 	@GetMapping("/login")
 	public String getLogInPage() {
 		return "Auth/login";
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> logIn(@RequestBody LoginRequest loginRequest) {
+		return ResponseEntity.ok(new LoginResponse("Login successful!", "Token"));
 	}
 
 }
