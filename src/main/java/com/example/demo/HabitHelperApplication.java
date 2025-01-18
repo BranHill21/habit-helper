d0730c0d283dfa14b4c9d40ee8e0747d9aba86f1
BranHill21 <bhill20@austincollege.edu>
2025-01-18 15:49:39 -0600

    fixing app.prop and .env

--- src/main/java/com/example/demo/HabitHelperApplication.java
+++ src/main/java/com/example/demo/HabitHelperApplication.java
@@ -3,10 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class HabitHelperApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(HabitHelperApplication.class, args);
 	}
 
